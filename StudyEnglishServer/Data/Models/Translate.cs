247bfd0cd9de0abc0ee0e4901fb12e93afec80ae
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:32:52 +0300

    Добавлена модель для перевода слова

---
+++ StudyEnglishServer/Data/Models/Translate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data.Models
+{
+    /// <summary>
+    /// Хранит перевод слова.
+    /// </summary>
+    public class Translate
+    {
+        /// <summary>
+        /// Идентификатор перевода.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Текст перевода.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// True, если это основное значение (перевод) слова.
+        /// </summary>
+        public bool IsPrimary { get; set; }
+    }
+}
