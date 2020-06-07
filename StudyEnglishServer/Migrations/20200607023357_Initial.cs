fa52a98f457822392b9dfd86f8b6fefe852be0e5
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:38:45 +0300

    Добавлены миграции

---
+++ StudyEnglishServer/Migrations/20200607023357_Initial.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudyEnglishServer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Words",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    Transcription = table.Column<string>(nullable: true),
+                    Association = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Words", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Audio",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Url = table.Column<string>(nullable: true),
+                    WordId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audio", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Audio_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudyHistory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Correct = table.Column<bool>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    WordId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudyHistory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudyHistory_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Translates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Text = table.Column<string>(nullable: true),
+                    IsPrimary = table.Column<bool>(nullable: false),
+                    WordId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Translates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Translates_Words_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Words",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Audio_WordId",
+                table: "Audio",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudyHistory_WordId",
+                table: "StudyHistory",
+                column: "WordId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Translates_WordId",
+                table: "Translates",
+                column: "WordId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audio");
+
+            migrationBuilder.DropTable(
+                name: "StudyHistory");
+
+            migrationBuilder.DropTable(
+                name: "Translates");
+
+            migrationBuilder.DropTable(
+                name: "Words");
+        }
+    }
+}
---
+++ StudyEnglishServer/Migrations/20200607023557_IncludeStatistics.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudyEnglishServer.Migrations
+{
+    public partial class IncludeStatistics : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StatisticsId",
+                table: "Words",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Statistics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CountShow = table.Column<int>(nullable: false),
+                    Correctness = table.Column<float>(nullable: false),
+                    DateLastShow = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statistics", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Words_StatisticsId",
+                table: "Words",
+                column: "StatisticsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Words_Statistics_StatisticsId",
+                table: "Words",
+                column: "StatisticsId",
+                principalTable: "Statistics",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Words_Statistics_StatisticsId",
+                table: "Words");
+
+            migrationBuilder.DropTable(
+                name: "Statistics");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Words_StatisticsId",
+                table: "Words");
+
+            migrationBuilder.DropColumn(
+                name: "StatisticsId",
+                table: "Words");
+        }
+    }
+}
