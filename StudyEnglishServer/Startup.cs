733881865f2fb67a50a50d6d4358f650558d8d0e
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:35:08 +0300

    Добавлена модель для истории изучения слова

--- StudyEnglishServer/Startup.cs
+++ StudyEnglishServer/Startup.cs
@@ -6,17 +6,22 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using StudyEnglishServer.Data;
 
 namespace StudyEnglishServer
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfigurationRoot confString;
+
+        public Startup(IHostingEnvironment hostEnv, IConfiguration configuration)
         {
+            confString = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
             Configuration = configuration;
         }
 
@@ -25,12 +30,15 @@ namespace StudyEnglishServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<StudyEnglishDBContext>(options => options.UseSqlServer(confString.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
