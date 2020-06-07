79ab89ecb24c71a8a723405391fa572c07c505f6
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:36:53 +0300

    Добавлена модель для параметров аудио-файла

---
+++ StudyEnglishServer/Data/Models/Audio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data.Models
+{
+    public class Audio
+    {
+        /// <summary>
+        /// Идентификатор файла
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Url файла
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
