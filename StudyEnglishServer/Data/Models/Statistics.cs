ced719550b8414929fd4127fa0be3d8e96fbc054
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:34:24 +0300

    Добавлена модель для статистики по изучению слова

---
+++ StudyEnglishServer/Data/Models/Statistics.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data.Models
+{
+    /// <summary>
+    /// Хранит статистику по изучению слова
+    /// </summary>
+    public class Statistics
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Количество показов
+        /// </summary>
+        public int CountShow { get; set; }
+        /// <summary>
+        /// Процент правильных ответов
+        /// </summary>
+        public float Correctness { get; set; }
+        /// <summary>
+        /// Дата последнего показа
+        /// </summary>
+        public DateTime DateLastShow { get; set; }
+    }
+}
