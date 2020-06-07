f2d71cb119454f785286c4928f10cea1bd3ba08b
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:36:36 +0300

    Добавлена модель для истории изучения слова

---
+++ StudyEnglishServer/Data/Models/StudyHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data.Models
+{
+    /// <summary>
+    /// Хранит историю изучения слова
+    /// </summary>
+    public class StudyHistory
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// True, если ответ был правильный
+        /// </summary>
+        public bool Correct { get; set; }
+        /// <summary>
+        /// Дата ответа
+        /// </summary>
+        public DateTime Data { get; set; }
+    }
+}
