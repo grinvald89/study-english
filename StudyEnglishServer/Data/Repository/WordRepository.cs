d43a124dc14a3ffc29b0c998387f203ee6235702
grinvald89 <v.greenwald@watcomdc.ru>
2020-06-07 06:42:34 +0300

    Добавлена настройка создания базы данных

---
+++ StudyEnglishServer/Data/Repository/WordRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyEnglishServer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data.Repository
+{
+    public class WordRepository
+    {
+        private readonly StudyEnglishDBContext dbContext;
+
+        public WordRepository(StudyEnglishDBContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IEnumerable<Word> Words =>
+                dbContext.Words
+                    .Include(c => c.Audio)
+                    .Include(c => c.Translate)
+                    .Include(c => c.Statistics)
+                    .Include(c => c.StudyHistory);
+        public IEnumerable<Translate> Translates => dbContext.Translates;
+        public IEnumerable<Audio> Audio => dbContext.Audio;
+        public IEnumerable<Statistics> Statistics => dbContext.Statistics;
+        public IEnumerable<StudyHistory> StudyHistory => dbContext.StudyHistory;
+    }
+}
---
+++ StudyEnglishServer/Data/StudyEnglishDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudyEnglishServer.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudyEnglishServer.Data
+{
+    public class StudyEnglishDBContext : DbContext
+    {
+        public StudyEnglishDBContext(DbContextOptions<StudyEnglishDBContext> options) : base(options) { }
+
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Translate> Translates { get; set; }
+        public DbSet<Audio> Audio { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
+        public DbSet<StudyHistory> StudyHistory { get; set; }
+    }
+}
